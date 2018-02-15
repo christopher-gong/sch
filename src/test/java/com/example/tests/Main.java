67ec46ac6da70e84c8fb9bfc4c85fd27eaaf5853
christopher-gong <christophergong@berkeley.edu>
2018-02-14 22:27:17 -0800

    working with input string from scheduleplanner, no error handling

--- src/test/java/com/example/tests/Main.java
+++ src/test/java/com/example/tests/Main.java
@@ -1,5 +1,7 @@
 package com.example.tests;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -16,6 +18,22 @@ public class Main {
             events[i].parse();
         }
         t.tearDown();
+    }
+
+    public static int[] inputClasses() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please copy your classes from SchedulePlaner!");
+        System.out.print("Classes: ");
+        String classInput = scanner.nextLine();
+        System.out.println(classInput);
+        classInput = classInput + ", helps end properly";
+        int numberOfClasses = StringUtils.countMatches(classInput, ",");
+        int[] classes = new int[numberOfClasses];
+        for (int i = 0; i < numberOfClasses; i++) {
+            classes[i] = Integer.parseInt(classInput.substring(classInput.indexOf(",") - 5, classInput.indexOf(",")));
+            classInput = classInput.substring(classInput.indexOf(",") + 1);
+        }
+        return classes;
     }
 
     public static void printer() throws Exception {
@@ -40,14 +58,13 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        //Scanner scanner = new Scanner(System.in);
-        //String classInput = scanner.nextLine();
+        int[] classes = inputClasses();
         //printer();
         //CalEvent c = new CalEvent("CS61B", "35432", "001-LEC", "Wheeler 150", "01/16/2018 - 05/04/2018", "MoWeFr 3:00PM - 3:59PM", "");
         //c.parse();
         //System.out.println(Arrays.toString(results));
         //int[] classes = {35432, 35439, 35478, 35915, 35897, 37205, 38343, 39838};
-        int[] classes = {35432, 35915, 39838};
+        //int[] classes = {35432, 35915, 39838};
         setup(classes);
         for (CalEvent e: events) {
             ClassAdder.addClass(e.calName, e.location, e.desc, e.startDateTime, e.endDateTime, e.recurrence, e.shortCourseName);
